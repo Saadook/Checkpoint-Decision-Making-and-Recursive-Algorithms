828384033915419b8ef6c000968cd235b4d66eb7
Sadok <medsadokghanouchi@gmail.com>
2024-08-15 09:54:13 +0100

    file added

---
+++ app.js
@@ -0,0 +1,27 @@
+
+  function IsLeapYear(year){
+    if (year %4 == 0 && year %100 !=0) or(year % 400==0);{
+
+        return true
+    } else {
+        return false
+    }
+  }
+  function fibonacci(n){
+  if(n<=1){
+  return n
+} else  {
+  return fibonacci(n-1) + fibonacci(n-2)
+}
+  }
+
+  function power(base, exponent){
+  if (exponent == 0){
+  return 1
+  }
+   exponent < 0
+  return 1 / power(base, -exponent)
+  else {
+  return base * power(base, exponent - 1)
+  }
+}
